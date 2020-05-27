9911544e524028636da03dad9d81aa2cf45f91d5
vladislav-kazakov <vkazakov@phys.nsu.ru>
2020-05-27 16:18:46 +0700

    new text on first page

--- frontend/views/default/index.php
+++ frontend/views/default/index.php
@@ -13,23 +13,19 @@ $this->title = Yii::$app->name;
 <div class="row">
     <div class="col-xs-12">
         <p>
-            <?= Yii::t('home', 'Culture as a social and historical phenomenon determines all individual interpretations and dominates the development of symbolic communication, the ways of communicating information through a system of cultural signs and symbols, through language and writing by Historic societies.') ?>
+            <?= Yii::t('home', 'Culture as a social and historical phenomenon determines all individual interpretations and dominates the development of symbolic communication, the ways of transferring information through a system of cultural signs and symbols, through language and writing in the later historic societies.') ?>
         </p>
         <p>
-            <?= Yii::t('home', 'Ethnographic research shows that such features as bundles of beads, necklaces, and bracelets as well as body art, scarification, tattooing, clothing and hairstyles are perceived by members of traditional societies as means of personal identification, indicating a person’s position within the group or marking the boundaries between the groups and neighboring communities. Other functions of personal ornaments may include gender or age markers, determiners of kinship, prestige or social status or they may be used for information exchange, etc.') ?>
+            <?= Yii::t('home', 'Ethnographic research shows that such elements as bunches of beads, necklaces, and bracelets as well as body art, scarification, tattooing, clothing etc. are considered by traditional societies members as means of personal identification, indicating the person’s position within the group or marking the boundaries between the groups and neighboring communities. Other functions of personal ornaments may include gender or age markers, determiners of kinship, prestige or social status or they may be used for information exchange, etc.') ?>
         </p>
         <p>
-            <?= Yii::t('home', 'The general picture of the Initial and Early Upper Paleolithic in Siberia is a mosaic combination of stone reduction technologies, bone and antler processing, as well as an amazing variety of fossil materials representing various species within the genus Homo. Forms of modern symbolic behavior look rather stable in the archaeological context of the Early Upper Paleolithic of northern Eurasia between 40,000-50,000 years ago. This is especially true of personal ornaments, which constitute symbolic nominal systems.') ?>
+            <?= Yii::t('home', 'The general picture of the Siberian Paleolithic is a mosaic combination of stone knapping technologies, bone and antler treatment, as well as the amazing variety of anthropological materials representing various species including the genus Homo. Forms of modern symbolic behavior look rather stable in the archaeological context of the Early Upper Paleolithic of northern Eurasia between 40,000 and 50,000 years ago. This context is especially true for personal ornaments which constitute symbolic nominal systems.') ?>
         </p>
         <p>
-            <?= Yii::t('home', 'The specific features of the Middle to Upper Paleolithic transition in southern Siberia include prolonged survival of Middle Paleolithic forms and convergence with the appearance and evolution of Aurignac-style elements. Forms, raw material, technologies, and types of personal ornaments in Siberian collections are not limited chronologically to the Initial and Early Upper Paleolithic.') ?>
+            <?= Yii::t('home', 'More than 50 archaeological sites dating back to the Initial and Early Upper Paleolithic have been studied in Mongolia, the Altai, the Cis-Baikal and Trans-Baikal, and Arctic Siberia. Today over 500 items made of bone, ivory, ostrich eggshell, seashells, and soft stone are represented in the Siberian and Far Eastern collections.') ?>
         </p>
         <p>
-            <?= Yii::t('home', 'More than 50 archaeological sites dating back to the Initial and Early Upper Paleolithic have been studied in Mongolia, the Altai, the Cis-Baikal and Trans-Baikal, and Arctic Siberia.') ?>
-        </p>
-        <p>
-            <?= Yii::t('home', 'A large collection of personal ornaments was acquired from more than 20 Siberian archaeological sites. Today over 300 objects made of bone, ivory, ostrich eggshell, seashells, and soft stone are present. These complexes date back to 25,000 – 30,000 to 40,000 – 50,000 years ago.') ?>
-
+            <?= Yii::t('home', 'Fundamental changes are taking place in the stylistics, technology and artistic images of ancient art due to the widespread dissemination of new materials - clay, metals in the Neolithic, Bronze and early Iron Age. The social function of the art objects is expanding. New meanings, concepts and mythology ideas appear.') ?>
         </p>
     </div>
 
